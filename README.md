14505b1cb8c0f377f4632f705af8a15ea5caff34
Ivor <ivorgrisel@gmail.com>
2023-10-27 13:11:51 +0200

    Tailwind setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # levv-consulting
+
+## Dev steps
+
+* When developing: ```npx tailwindcss -i ./packages/levv/levv_theme/css/input.css -o ./packages/standard/lev/dist/style.min.css --watch --minify```
---
+++ tailwind.config.js
@@ -0,0 +1,49 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./packages/levv/levv_theme/**/*.{php,html,js}"],
+  theme: {
+    extend: {
+      // fontFamily: {
+      //   'revicons': ['revicons'],
+      //   'lato': ['Lato'],
+      //   'logo': ['"Oooh Baby"'],
+      // },
+      colors: {
+        'levv-primary': '#6096B4',
+        'levv-secondary': '#93BFCF',
+        'levv-tertiary': '#BDCDD6',
+        'levv-quaternary': '#EEE9DA'
+      },
+      // boxShadow: {
+      //   '3xl': '0 8px 6px -6px rgba(0, 0, 0, 0.1)',
+      // },
+      // maxWidth: {
+      //   '3/4': '75%',
+      // },
+      // spacing: {
+      //   '128': '32rem',
+      //   '30': '7.5rem',
+      //   'header': '7.5rem',
+      //   'slider': 'calc(100vh - 7.5rem)',
+      //   'mobile-header': '5rem',
+      //   'mobile-slider': 'calc(100vh - 5rem)'
+      // },
+      // scrollMargin: {
+      //   '30': '7.5rem',
+      //   'header': '10rem',
+      //   'mobile-header': '5.5rem'
+      // },
+      // gridTemplateRows: {
+      //   '12': 'repeat(12, minmax(0, 1fr))',
+      // },
+      // gridRowEnd: {
+      //   '11': '11',
+      //   '13': '13',
+      // },
+      // rotate: {
+      //   '270': '270deg'
+      // }
+    }
+  },
+  // plugins: [require("daisyui")],
+}
