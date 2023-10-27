9ce5d041eb0508eee37a13ff10260063aaa86441
Ivor <ivorgrisel@gmail.com>
2023-10-27 13:33:53 +0200

    Tailwind and theme settings

---
+++ automad-master/packages/levv/levv_theme/elements/footer.php
@@ -0,0 +1,34 @@
+<footer id="footer" class="w-full flex flex-col items-center mt-24
+            md:flex-row">
+            <div class="w-full bg-svj-tertiary text-black text-xl flex items-center justify-center h-32
+            md:w-1/2 text-center">
+                <span>&copy; 2022<?php $currentYear = date("Y");
+                        if ($currentYear != 2022) {
+                            echo "-".$currentYear;
+                        }
+                    ?> <strong>LEVV</strong>. Alle Rechten Voorbehouden</span>
+            </div>
+            <div class="w-full bg-svj-primary text-white text-xl flex items-center justify-center h-32
+            md:w-1/2 md:flex">
+                <button class="flex items-center justify-center gap-2" onclick="scrollToTop()">Naar boven<i class="arrow_carrot-up_alt2"></i></button>
+        </footer>
+        <!-- <script>
+            function scrollToTop() {
+                window.scrollTo(0, 0);
+            }
+        </script> -->
+        <@ if @{ :origUrl } @>
+            <!-- <script src="/packages/@{theme}/js/homepage_slider.js" async></script>
+            <script src="/packages/@{theme}/js/contact_form.js" async></script>
+            <script src="/packages/@{theme}/js/supervisor_modal.js" async></script>    -->
+        <@ else @>
+            <script src="/packages/@{theme}/js/automatic_redirect.js" async></script>          
+        <@ end @>
+        <!-- <script>
+            const links = document.querySelectorAll('[href*="://"]');
+            Array.from(links).forEach((link) => {
+                link.setAttribute('target', '_blank');
+            });
+        </script> -->
+    </body>
+</html>
