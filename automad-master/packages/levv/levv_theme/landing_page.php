69bea1877bb6c1f8870b9276da96658d52de5984
Ivor <ivorgrisel@gmail.com>
2024-07-19 13:35:11 +0200

    Mostly text size adjustments

--- automad-master/packages/levv/levv_theme/landing_page.php
+++ automad-master/packages/levv/levv_theme/landing_page.php
@@ -7,7 +7,7 @@ The snippet can be overriden before including the actual template in order to ex
 #>
 <main class="w-full flex flex-col items-center">
 	<div id="levv-question" class="w-full flex flex-col items-center fixed mt-32 bg-levv-klei-header-background">
-		<h1 class="text-2xl lg:text-5xl p-2 lg:p-0"><span>Welke uitdaging wil je aangaan </span><span class="text-levv-wijnrood font-bold text-3xl lg:text-6xl">met Levv?</span></h1>
+		<h1 class="text-xl lg:text-5xl p-2 lg:p-0"><span>Welke uitdaging wil je aangaan </span><span class="text-levv-wijnrood font-bold text-2xl lg:text-6xl">met Levv?</span></h1>
 		<ul class="flex flex-wrap gap-2 lg:gap-6 m-4 justify-center">
 			<@ newPagelist {
 				type: 'children',
@@ -15,7 +15,7 @@ The snippet can be overriden before including the actual template in order to ex
 				context: "/homepage-sections",
 			} @>
 			<@ foreach in pagelist @>
-				<li class="group text-xl lg:text-3xl grid grid-cols-levv-homepage-section-filter grid-rows-levv-homepage-section-filter transition ease-in-out delay-125 hover:cursor-pointer hover:scale-105 focus:scale-105">
+				<li class="group text-sm lg:text-3xl grid grid-cols-levv-homepage-section-filter grid-rows-levv-homepage-section-filter transition ease-in-out delay-125 hover:cursor-pointer hover:scale-105 focus:scale-105">
 					<div class="col-start-1 col-end-4 row-start-1 row-end-4 w-1/2 relative bg-levv-klei-background h-[10px] -top-[5px] justify-self-center"></div>
 					<button value="levv-section-@{ :i }" class="col-start-1 col-end-3 row-start-1 row-end-3 text-center font-passageway-light border-solid border-4 rounded-tl-xl rounded-br-xl uppercase p-3 transition ease-in-out delay-125 group-hover:border-levv-wijnrood" onclick="scrollToSection(this.value)">@{ title }</button>
 				</li>
@@ -28,17 +28,11 @@ The snippet can be overriden before including the actual template in order to ex
 			<div class="col-start-1 col-span-1 row-start-2 row-end-3 border-solid border-b-4 border-levv-korenblauw"></div>
 			<div class="col-start-2 col-span-2 row-start-2 row-end-3 border-solid border-r-4 border-b-4 rounded-br-xl  border-levv-korenblauw"></div>
 			<div class="col-start-3 col-span-2 row-start-1 row-end-2 border-solid border-l-4 border-t-4 rounded-tl-xl  border-levv-korenblauw"></div>
-			<!-- <div class="col-start-5 col-spen-1 row-start-1 row-end-4 w-1/3 relative bg-levv-klei-background h-[10px] -top-[5px] justify-self-center"></div> -->
-			<div class="col-start-5 col-span-1 row-start-1 row-end-2 border-solid border-t-4 p-6 border-levv-korenblauw">
-				<h2 class="text-3xl">@{ title } <span class="text-levv-wijnrood font-bold">met Levv</span></h2>
-				<p>@{ +cornerstone_description }</p>
+			<div class="col-start-5 col-span-3 md:col-start-5 md:col-span-1 row-start-1 row-end-2 border-solid border-t-4 p-6 border-levv-korenblauw">
+				<h2 class="text-xl lg:text-3xl">@{ title } <span class="text-levv-wijnrood font-bold">met Levv</span></h2>
+				<div class="text-base lg:text-xl">@{ +cornerstone_description }</div>
 			</div>
 			<div class="col-start-6 col-span-2 row-start-1 row-end-2 border-solid border-t-4  border-levv-korenblauw"></div>
-			<!-- <div class="col-start-1 col-end-4 row-start-1 row-end-4 w-1/2 relative bg-levv-klei-background h-[10px] -top-[5px] justify-self-center"></div>
-			<div class="col-start-1 col-end-3 row-start-1 row-end-3 border-solid border-4 rounded-tl-xl rounded-br-xl p-3 border-levv-korenblauw">
-				<h2 class="text-2xl">@{ title } <span class="text-levv-wijnrood font-bold text-3xl">met Levv</span></h2>
-				<p>@{ +cornerstone_description }</p>
-			</div> -->
 		</div>
 	<@ end @>
 	
