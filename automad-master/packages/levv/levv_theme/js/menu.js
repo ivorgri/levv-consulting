aea3fa8d19c42653c85f7f878c27a088bce1a1b1
Ivor <ivorgrisel@gmail.com>
2024-11-29 23:30:35 +0100

    changed-everything

--- automad-master/packages/levv/levv_theme/js/menu.js
+++ automad-master/packages/levv/levv_theme/js/menu.js
@@ -4,6 +4,7 @@ const menuItemsArray = document.getElementsByClassName("levv-menu-opened");
 
 toggleMenuButton.addEventListener("click",()=>{
     toggleMenuButton.classList.toggle('opened')
+    navBar.classList.toggle('bg-levv-klei-background')
     navBar.classList.toggle('levv-nav-bar-closed')
     navBar.classList.toggle('levv-nav-bar-opened')
     for (let menuItem of menuItemsArray) {
